cef55dd1f6ee52eb519fb6fff25d1331990d9856
Alexandru Clapou <alexandru.clapou@golfgenius.com>
2022-02-24 18:28:11 +0200

    added user_scores route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     post 'login', to: 'users#login'
     get 'feed', to: 'scores#user_feed'
     get 'user_scores/:id', to: 'users#user_scores'
-    resources :users, only: %i[show]
     resources :scores, only: %i[create destroy]
   end
 end
